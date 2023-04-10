c0ebd5762db845b91689fefbac3824474d815a9d
WladyslawK <vladkurr7890@gmail.com>
2023-04-10 22:34:32 +0200

    feature: (AppRoutes) AppRoutes

---
+++ src/routing/AppRoutes.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Navigate, Route, Routes} from "react-router-dom";
+import {PATH} from "../utils/routePaths.enum";
+import {ProductsPage} from "../pages/ProductsPage";
+import {About, Home} from "../pages";
+
+export const AppRoutes = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Navigate to={PATH.HOME} />} />
+        <Route path={PATH.HOME} element={<Home/>}/>
+        <Route path={PATH.ABOUT} element={<About/>} />
+        <Route path={PATH.PRODUCTS} element={<ProductsPage/>} />
+      </Routes>
+    </>
+  );
+};
