8bb7036c6a5dcd5ab6a035d6bd38c23dfdeb9dd2
WladyslawK <vladkurr7890@gmail.com>
2023-05-02 21:50:00 +0200

    feature: (productReducer) productReducer

--- src/reducers/product-reducer.ts
+++ src/reducers/product-reducer.ts
@@ -1,70 +1,9 @@
-import {comfyAPI, ProductResponseType} from "../api/comfy-api";
-import {PRODUCTS_ERROR, PRODUCTS_LOADING, SET_PRODUCTS} from "../utils/actions";
-import {useDispatch} from "react-redux";
-import {Dispatch} from "redux";
+  
 
-export type productsStateType = {
-  products_loading: boolean
-  products_error: boolean
-  products: ProductResponseType[],
-  feature_products: ProductResponseType[],
-  single_product_loading: boolean
-  single_product_error: boolean
-  single_product: {}
-}
-
-export const productsInitialState: productsStateType = {
-  products_loading: false,
-  products_error: false,
-  products: [],
-  feature_products: [],
-  single_product_loading: false,
-  single_product_error: false,
-  single_product: {}
-}
-
-export const productReducer = (state = productsInitialState, action: ActionsType
-):
-  productsStateType => {
-  switch (action.type) {
-
-    case PRODUCTS_LOADING:
-      return {...state, products_loading: true}
-
-    case SET_PRODUCTS: {
-      const featuredProducts = action.payload.products.filter(product => product.shipping === true)
-      return {...state, products: action.payload.products, products_loading: false, feature_products: featuredProducts}
+export const ProductReducer =
+  (state: any, action: any) => {
+    switch (action.type) {
+      default:
+        return state
     }
-
-    case PRODUCTS_ERROR:
-      return {...state, products_error: true}
   }
-
-  return state
-}
-
-type ActionsType =
-  ReturnType<typeof setProductsAC> |
-  ReturnType<typeof setProductsError> |
-  ReturnType<typeof setProductsLoading>
-
-
-export const getProductsTC = () => async (dispatch: Dispatch) => {
-  dispatch(setProductsLoading())
-  try {
-    const data = await comfyAPI.getProducts()
-    if (data.data) {
-      dispatch(setProductsAC(data.data))
-    }
-
-  } catch
-    (error: any) {
-    console.log(error)
-  }
-}
-
-export const setProductsAC = (products: ProductResponseType[]) => ({type: SET_PRODUCTS, payload: {products}} as const)
-
-export const setProductsError = () => ({type: PRODUCTS_ERROR} as const)
-
-export const setProductsLoading = () => ({type: PRODUCTS_LOADING} as const)
