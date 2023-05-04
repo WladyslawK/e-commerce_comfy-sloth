e439e756cc825bcdbbb016a4c57ef32d0f486bd7
WladyslawK <vladkurr7890@gmail.com>
2023-05-04 23:55:17 +0200

    refactor: products-reducer

--- src/reducers/products-reducer.ts
+++ src/reducers/products-reducer.ts
@@ -1,70 +1,73 @@
-import {comfyAPI, ProductsResponseType} from "../api/comfy-api";
-import {PRODUCTS_ERROR, PRODUCTS_LOADING, SET_PRODUCTS} from "../utils/actions";
-import {useDispatch} from "react-redux";
-import {Dispatch} from "redux";
+import { comfyAPI, ProductsResponseType } from "../api/comfy-api";
+import {
+  PRODUCTS_ERROR,
+  PRODUCTS_LOADING,
+  SET_PRODUCTS,
+} from "../utils/actions";
+import { useDispatch } from "react-redux";
+import { Dispatch } from "redux";
 
 export type productsStateType = {
-  products_loading: boolean
-  products_error: boolean
-  products: ProductsResponseType[],
-  feature_products: ProductsResponseType[],
-  single_product_loading: boolean
-  single_product_error: boolean
-  single_product: {}
-}
+  products_loading: boolean;
+  products_error: boolean;
+  products: ProductsResponseType[];
+  feature_products: ProductsResponseType[];
+};
 
 export const productsInitialState: productsStateType = {
   products_loading: false,
   products_error: false,
   products: [],
   feature_products: [],
-  single_product_loading: false,
-  single_product_error: false,
-  single_product: {}
-}
+};
 
-export const productsReducer = (state = productsInitialState, action: ActionsType
-):
-  productsStateType => {
+export const productsReducer = (
+  state = productsInitialState,
+  action: ActionsType
+): productsStateType => {
   switch (action.type) {
-
     case PRODUCTS_LOADING:
-      return {...state, products_loading: true}
+      return { ...state, products_loading: true };
 
     case SET_PRODUCTS: {
-      const featuredProducts = action.payload.products.filter(product => product.shipping === true)
-      return {...state, products: action.payload.products, products_loading: false, feature_products: featuredProducts}
+      const featuredProducts = action.payload.products.filter(
+        (product) => product.shipping === true
+      );
+      return {
+        ...state,
+        products: action.payload.products,
+        products_loading: false,
+        feature_products: featuredProducts,
+      };
     }
 
     case PRODUCTS_ERROR:
-      return {...state, products_error: true}
+      return { ...state, products_error: true };
   }
 
-  return state
-}
+  return state;
+};
 
 type ActionsType =
-  ReturnType<typeof setProductsAC> |
-  ReturnType<typeof setProductsError> |
-  ReturnType<typeof setProductsLoading>
-
+  | ReturnType<typeof setProductsAC>
+  | ReturnType<typeof setProductsError>
+  | ReturnType<typeof setProductsLoading>;
 
 export const getProductsTC = () => async (dispatch: Dispatch) => {
-  dispatch(setProductsLoading())
+  dispatch(setProductsLoading());
   try {
-    const data = await comfyAPI.getProducts()
+    const data = await comfyAPI.getProducts();
     if (data.data) {
-      dispatch(setProductsAC(data.data))
+      dispatch(setProductsAC(data.data));
     }
-
-  } catch
-    (error: any) {
-    console.log(error)
+  } catch (error: any) {
+    console.log(error);
   }
-}
+};
 
-export const setProductsAC = (products: ProductsResponseType[]) => ({type: SET_PRODUCTS, payload: {products}} as const)
+export const setProductsAC = (products: ProductsResponseType[]) =>
+  ({ type: SET_PRODUCTS, payload: { products } } as const);
 
-export const setProductsError = () => ({type: PRODUCTS_ERROR} as const)
+export const setProductsError = () => ({ type: PRODUCTS_ERROR } as const);
 
-export const setProductsLoading = () => ({type: PRODUCTS_LOADING} as const)
+export const setProductsLoading = () => ({ type: PRODUCTS_LOADING } as const);
