f0ff5fb911666bf87d40ba99a7bdde069cccdda8
WladyslawK <vladkurr7890@gmail.com>
2023-05-27 21:46:24 +0200

    feature: Grid view for ProductList

---
+++ src/components/ProductsList.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {useSelector} from "react-redux";
+import {FilterStateType} from "../reducers/filter-reducer";
+import {rootReducerType} from "../store/store";
+import {GridView} from "./GridView";
+
+export function ProductsList() {
+
+  const {filtered_products: products} = useSelector<rootReducerType, FilterStateType>(state => state.filter)
+
+  if(!products.length){
+    return <h5 style={{textTransform: 'none'}}>
+      Sorry no products match your search...
+    </h5>
+  }
+
+  return (
+    <div>
+      <GridView products={products}/>
+    </div>
+  );
+};
